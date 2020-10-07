1b1def5ad0b5b343d692cdc16c6e0fa5fe7d73fe
rabah babaci <4rabah@gmail.com>
2020-10-06 18:57:53 -0700

    .

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,8 +1,9 @@
 """CPU functionality."""
 
 import sys
+import threading
 
-LDI, PRN, HALT, MUL, ADD, PUSH, POP, JMP, ST, PRA = 0b10000010, 0b01000111, 0b00000001, 0b10100010, 0b10100000, 0b01000101, 0b01000110, 0b01010100, 0b10000100, 0b01001000
+LDI, PRN, HALT, MUL, ADD, PUSH, POP, JMP, ST, PRA, IRET = 0b10000010, 0b01000111, 0b00000001, 0b10100010, 0b10100000, 0b01000101, 0b01000110, 0b01010100, 0b10000100, 0b01001000, 0b00010011
 
 
 class CPU:
@@ -21,6 +22,8 @@ class CPU:
         self.SP = 7  # stack pointer
         self.IR = None
         self.FL = None
+        self.IM = 5  # interrupt mask
+        self.IS = 6  # interrupt status
         self.branch_table = {  # a table to store the helpers for fast lookup
             LDI: self.handle_LDI,
             PRN: self.handle_PRN,
@@ -31,16 +34,13 @@ class CPU:
             POP: self.handle_POP,
             JMP: self.handle_JMP,
             ST: self.handle_ST,
-            PRA: self.handle_PRA
+            PRA: self.handle_PRA,
+            IRET: self.handle_IRET
         }
 
     def isKthBitSet(self, n, k):
         if n & (1 << (k - 1)):
             return True
-
-    def getOpcodeHex(self, opcode):
-        opcode = hex(opcode)
-        return int(opcode[2:])
 
     def load(self, program):
         """Load a program into memory."""
@@ -85,7 +85,6 @@ class CPU:
     def handle_LDI(self, *args):
         reg_num, value = args[0], args[1]
         self.registers[reg_num] = value
-        # print("LDI")
 
     def handle_PRN(self, *args):
         reg_num = args[0]
@@ -95,7 +94,7 @@ class CPU:
 
     def handle_HALT(self, *args):
         self.halted = True
-        # print("HALT")
+        print("HALT")
 
     def handle_MUL(self, *args):
         reg_a, reg_b = args[0], args[1]
@@ -108,7 +107,7 @@ class CPU:
     def handle_PUSH(self, *args):
         # stack starts at last memory index
         if self.stack_is_empty:
-            self.registers[self.SP] = len(self.ram)
+            self.registers[self.SP] = len(self.ram) - 8
             self.stack_is_empty = False
 
         self.registers[self.SP] -= 1
@@ -133,10 +132,58 @@ class CPU:
     def handle_PRA(self, *args):
         print(chr(self.registers[args[0]]))
 
+    def handle_IRET(self, *args):
+        s_registers = self.ram_read(self.registers[self.SP])
+        self.registers[self.SP] += 1
+        s_FL = self.ram_read(self.registers[self.SP])
+        self.registers[self.SP] += 1
+        s_PC = self.ram_read(self.registers[self.SP])
+        self.registers[self.SP] += 1
+
+        self.registers = s_registers + self.registers[6:]
+        self.FL = s_FL
+        self.PC = s_PC
+
+    def run_timer_interrupt(self):
+        self.registers[self.IS] |= 1  # 1- set bit 0 in the IS
+
+    def clear_bit(self, position):
+        mask = 1 << position
+        self.registers[self.IS] &= ~mask
+
+    def stack_cpu_state(self):
+        if self.stack_is_empty:
+            self.registers[self.SP] = len(self.ram) - 8
+            self.stack_is_empty = False
+
+        for i in [self.PC, self.FL, self.registers[:6]]:
+            self.registers[self.SP] -= 1
+            self.ram_write(i, self.registers[self.SP])
+
     def run(self):
         """Run the CPU."""
+        # run the timer interrupt
+        threading.Timer(1, self.run_timer_interrupt).start()
 
         while not self.halted:
+            for i in range(8):
+                if (self.registers[self.IS] >> i) & 1 == 1:
+                    masked_interrupts = self.registers[self.IM] & self.registers[self.IS]
+
+                    for k in range(8):
+                        interrupt_happend = ((masked_interrupts >> k) & 1) == 1
+                        if interrupt_happend:
+                            # do your magic, and stop further checking
+                            # 1- disable further interrupts
+
+                            # 2- clear the bit in the IS register
+                            self.clear_bit(i)
+                            # 3- store the state of the cpu
+                            self.stack_cpu_state()
+                            # set PC to the interrupt handler address
+                            self.PC = self.ram_read(248 + i)
+                            break
+
             if self.address == self.registers[self.SP]:
                 print("*** IT'S TIME TO EXIT, THE STACK IS ABOUT TO OVER FLOW ***")
                 return
@@ -147,7 +194,9 @@ class CPU:
             operand_b = self.ram_read(self.PC + 2)
 
             self.branch_table[IR](operand_a, operand_b)
-            self.PC += num_operands
+
+            if IR != JMP and IR != IRET:
+                self.PC += num_operands
 
     def ram_read(self, MAR):
         """should accept the address to read and return the value stored there"""
